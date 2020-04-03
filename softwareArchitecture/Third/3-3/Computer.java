e42bc30a382e49bab5e828eb312e1007f461ea6a
Nam <52815908+NamSuJi@users.noreply.github.com>
2020-04-03 19:20:40 +0900

    Update Computer.java

--- softwareArchitecture/Third/3-3/Computer.java
+++ softwareArchitecture/Third/3-3/Computer.java
@@ -1,19 +1,21 @@
 package FacadePattern;
 
-
 public class Computer {
 	private CPU cpu;
 	private Memory memory;
 	private HardDrive hardDrive;
+	private Done done;
 	
 	public Computer() {
 		this.cpu = new CPU();
 		this.memory = new Memory();
 		this.hardDrive = new HardDrive();
+		this.done = new Done();
 	}
 	public void run() {
 		cpu.processData();
 		memory.load();
 		hardDrive.readData();
+		done.notice();
 	}
 }
